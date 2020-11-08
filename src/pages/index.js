cbfb98542642a05c454b284b563818491f057041
Eka Prasetia <ekaone3033@gmail.com>
2020-11-08 18:09:31 +0700

    add config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+// import "./mystyles.scss"
 
 const IndexPage = () => (
-  <Layout>
+  <div className="container">
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="columns">
+      <div className="column">
+        <h2 className="title is-2">Level 2 heading</h2>
+        <p className="content">Cool content. Using Bulma!</p>
+      </div>
+      <div className="column is-four-fifths">
+        <h2 className="title is-2">Level 2 heading</h2>
+        <p className="content">This column is cool too!</p>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
